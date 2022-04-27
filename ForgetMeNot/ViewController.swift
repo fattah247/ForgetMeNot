0baf397c235356b54499a38ac42bf3be521eb042
fattah247 <muhammad.abdul92@ui.ac.id>
2022-04-27 10:01:00 +0700

    Initial Commit

---
+++ ForgetMeNot/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ForgetMeNot
+//
+//  Created by Muhammad Abdul Fattah on 27/04/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
